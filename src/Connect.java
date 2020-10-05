644fc22eca4571d6e4ee557a930ba8d146ae5137
Yura Lug <LUV97@ukr.net>
2020-10-05 23:53:38 +0300

    Database

---
+++ src/Connect.java
@@ -0,0 +1,44 @@
+import java.io.FileWriter;
+import java.io.IOException;
+import java.sql.*;
+
+public class Connect {
+    public static void main(String[] args) throws ClassNotFoundException, SQLException, IOException {
+        String url = "jdbc:mysql://localhost:3306/Shop?serverTimezone=UTC";
+        String userName = "root";
+        String password = "qwerty123";
+
+        Class.forName("com.mysql.cj.jdbc.Driver");
+
+        Connection conn = DriverManager.getConnection(url, userName, password);
+
+        System.out.println("Connection Succesful");
+
+        Statement statement = conn.createStatement();
+
+        String sql;
+        sql = "SELECT * FROM shoppers";
+
+        ResultSet resultSet = statement.executeQuery(sql);
+        try(FileWriter writer = new FileWriter("notes3.txt", false)) {
+            while (resultSet.next()) {
+                int id = resultSet.getInt("id");
+                String name = resultSet.getString("name");
+                String specialty = resultSet.getString("specialty");
+                int salary = resultSet.getInt("salary");
+                String ss = "\\r\\n";
+                writer.write(id + " ");
+                writer.write(name + " ");
+                writer.write(specialty + " ");
+                writer.write(salary);
+                writer.write(ss);
+
+
+            }
+        }
+        resultSet.close();
+        statement.close();
+        conn.close();
+
+    }
+}
